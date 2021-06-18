331b930ff8e1109c54d422b5d5ec6811c13466aa
Tianhao <tianhaoz@umich.edu>
2021-06-18 05:22:53 +0000

    feat: server half finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,35 @@
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
+import 'package:grpc/grpc.dart' as grpc;
+import 'package:flutter_local_grpc_server/generated/counter.pbgrpc.dart';
+
+class CounterService extends CounterServerServiceBase {
+  @override
+  Future<CounterResponse> execAction(
+      grpc.ServiceCall call, CounterRequest request) async {
+    CounterResponse res = CounterResponse();
+    res.msg = "ack";
+    return res;
+  }
+}
+
+void runCounterService(String msg) async {
+  final server = grpc.Server([CounterService()]);
+  await server.serve(port: 50051);
+  print('Server listening...');
+}
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -30,15 +40,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -47,50 +48,45 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List isolates = [];
+  late grpc.ClientChannel channel;
+  late CounterServerClient client;
 
-  void _incrementCounter() {
+  void initState() {
+    super.initState();
+    startIsolates();
+    channel = grpc.ClientChannel('10.0.2.2',
+        port: 50051,
+        options: const grpc.ChannelOptions(
+            credentials: grpc.ChannelCredentials.insecure()));
+    client = CounterServerClient(channel,
+        options: grpc.CallOptions(timeout: Duration(seconds: 30)));
+  }
+
+  void startIsolates() async {
+    isolates.add(await Isolate.spawn(runCounterService, "start"));
+    print("Server launched");
+  }
+
+  void _incrementCounter() async {
+    CounterRequest req = CounterRequest();
+    req.type = CounterRequest_Type.Increase;
+    req.msg = "add";
+    CounterResponse res = await client.execAction(req);
+    print(res.msg);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
